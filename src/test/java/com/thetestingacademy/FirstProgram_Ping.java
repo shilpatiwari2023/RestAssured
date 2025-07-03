98ef94589fa68753e27dd31be64a01f865064480
Shilpa Tiwari <shilpatiwari08@gmail.com>
2025-07-03 18:43:15 +0530

    API 3 july

--- src/test/java/com/thetestingacademy/FirstProgram_Ping.java
+++ src/test/java/com/thetestingacademy/FirstProgram_Ping.java
@@ -2,7 +2,7 @@ package com.thetestingacademy;
 
 import io.restassured.RestAssured;
 
-public class HelloWorld {
+public class FirstProgram_Ping {
     public static void main(String[] args) {
         RestAssured.given()
                       .baseUri("https://restful-booker.herokuapp.com/ping")
